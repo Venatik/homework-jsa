96e84355b6c1e21cad862f1384f784d5324611c1
Venatik <stefan.trajkovski008@gmail.com>
2024-02-21 09:43:54 +0100

    new file:   JS Advanced - Class 14/index.html 	new file:   JS Advanced - Class 14/script.js

---
+++ JS Advanced - Class 14/script.js
@@ -0,0 +1,41 @@
+// Get bordering countries function
+// Using async fetch, call the countries API for a country by code.
+// Create a function that gets all the neighbouring countries objects and returns them in the console
+
+// With getting an array:
+// async function getBorderingCountries(code) {
+//     const url = `https://restcountries.com/v2/alpha/${code}`;
+//     try {
+//         const response = await fetch(url);
+//         const country = await response.json();
+//         const borderingCountries = country.borders;
+//         console.log(borderingCountries);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// With getting the objects:
+async function getBorderingCountries(code) {
+    const url = `https://restcountries.com/v2/alpha/${code}`;
+    try {
+        const response = await fetch(url);
+        const country = await response.json();
+        console.log("Country:", country);
+        const borderingCountries = country.borders;
+
+        for (let borderCode of borderingCountries) {
+            const borderUrl = `https://restcountries.com/v2/alpha/${borderCode}`;
+            const borderResponse = await fetch(borderUrl);
+            const borderCountry = await borderResponse.json();
+            console.log("Neighbour:", borderCountry);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+getBorderingCountries("MKD");
+// getBorderingCountries("SRB");
+// getBorderingCountries("ALB");
